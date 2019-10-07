a6ff7f3ae1229d6992c440b1e1bf01edfba0910d
Peter Milne <Peter.Milne@adform.com>
2019-10-07 16:12:55 +0200

    remove spark code and replace with git submodule from https://github.com/big-data-europe/docker-spark.git

--- spark/build-spark-images.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker build -t spark-base ./spark-base
-docker build -t spark-master ./spark-master
-docker build -t spark-worker ./spark-worker
-docker build -t spark-submit ./spark-submit
