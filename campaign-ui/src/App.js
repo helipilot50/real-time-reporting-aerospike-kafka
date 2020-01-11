911727654a9fb646e05a9ed71ddc20aaddaab4ab
Peter Milne <Peter.Milne@adform.com>
2020-01-11 14:28:45 +0100

    Added material UI components

--- campaign-ui/src/App.js
+++ campaign-ui/src/App.js
@@ -2,8 +2,29 @@ import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider, useQuery } from '@apollo/react-hooks';
 import { gql } from "apollo-boost";
-import logo from './logo.svg';
 import './App.css';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  table: {
+    minWidth: 650,
+  },
+});
 
 
 const client = new ApolloClient({
@@ -28,27 +49,64 @@ const CAMPAIGN_DETAIL = gql`
 
 
 function CampaignSet() {
+  const classes = useStyles();
   const { loading, error, data } = useQuery(CAMPAIGN_DETAIL);
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :( {error}</p>;
   console.log(data);
-  return data.campaigns.map((campaign) => (
-    <div key={campaign.id}>
-      <p>
-        id: {campaign.id}
-      </p>
-      <p>
-        name: {campaign.name}
-      </p>
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Id</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Impressions</TableCell>
+            <TableCell align="right">Clicks</TableCell>
+            <TableCell align="right">Visits</TableCell>
+            <TableCell align="right">Conversions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.campaigns.map(campaign => (
+            <TableRow key={campaign.id}>
+              <TableCell component="th" scope="campaign">
+                {campaign.id}
+              </TableCell>
+              <TableCell align="right">{campaign.name}</TableCell>
+              <TableCell align="right">{campaign.aggregateKPIs.impressions}</TableCell>
+              <TableCell align="right">{campaign.aggregateKPIs.clicks}</TableCell>
+              <TableCell align="right">{campaign.aggregateKPIs.visits}</TableCell>
+              <TableCell align="right">{campaign.aggregateKPIs.conversions}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+function CampaignAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+            Campaigns
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
-  ));
-}
+  );
+};
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <h2>Campaign Application</h2>
+        <CampaignAppBar />
         <CampaignSet />
       </div>
     </ApolloProvider >
