15d201a1e63d101fae778b53e49292d4527aed15
Peter Milne <Peter.Milne@adform.com>
2020-01-29 12:43:43 +0100

    no message

---
+++ campaign-ui/src/components/shake.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { headShake } from 'react-animations';
+import { StyleSheet, css } from 'aphrodite';
+const styles = StyleSheet.create({
+  headShake: {
+    animationName: headShake,
+    animationDuration: '1s'
+  }
+});
+const makeValidationErrorAnimation = (Target) => {
+  return class extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { shouldShake: false };
+    }
+
+    onClick = () => {
+      this.setState({ shouldShake: true }, () => {
+        const self = this;
+        setTimeout(() => self.setState({ shouldShake: false }), 1000);
+      });
+    };
+
+    render() {
+      return (
+        <Target isOpen={true}
+          onClick={this.onClick}
+          additionalStyles={{ text: {}, frame: {} }}
+          frameClass={this.state.shouldShake ? css(styles.headShake) : ''} />
+      );
+    }
+  }
+};
+export default makeValidationErrorAnimation;
+
+
+// see https://www.freecodecamp.org/news/how-to-build-animated-microinteractions-in-react-aab1cb9fe7c8/
