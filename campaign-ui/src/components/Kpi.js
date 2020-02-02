263af992fd9a3c3f3e1269af5e947d485fc762ca
Peter Milne <Peter.Milne@adform.com>
2020-02-02 20:43:30 +0100

    added wiggle to Kpi -- doesn't work yet

--- campaign-ui/src/components/Kpi.js
+++ campaign-ui/src/components/Kpi.js
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { Subscription } from 'react-apollo';
-
+import { headShake } from 'react-animations';
+import { StyleSheet, css } from 'aphrodite';
 import gql from 'graphql-tag';
 
 
+const styles = StyleSheet.create({
+  headShake: {
+    animationName: headShake,
+    animationDuration: '1s'
+  }
+});
 
 const useStyles = makeStyles(theme => ({
   card: {
@@ -20,6 +27,7 @@ const useStyles = makeStyles(theme => ({
 }));
 
 
+
 const KPI_SUBSCRIPTION = gql`
 subscription kpiUpdate($campaignId: ID!, $kpiName:String!){
   kpiUpdate(campaignId: $campaignId, kpiName: $kpiName) {
@@ -30,26 +38,51 @@ subscription kpiUpdate($campaignId: ID!, $kpiName:String!){
 }
 `;
 
-export const Kpi = ({ campaignId, kpiName, initialValue }) => {
-  const classes = useStyles();
-  return (
-    <Card className={classes.card} variant="outlined">
-      <CardContent>
-        <Typography>
-          <Subscription subscription={KPI_SUBSCRIPTION} variables={{ campaignId, kpiName }} shouldResubscribe={true}>
-            {
-              ({ data, loading }) => {
-                if (data) {
 
-                  return (data.kpiUpdate.value);
+export class Kpi extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      campaignId: props.campaignId,
+      kpiName: props.kpiName,
+      initialValue: props.initialValue,
+      startShake: false
+    }
+    this.wiggle = this.wiggle.bind(this);
+  }
+
+  wiggle(something) {
+    this.setState({ startShake: true })
+    console.log('wiggle', something);
+    setTimeout(() => this.setState({ startShake: false }), 1000);
+  }
+
+  render() {
+
+    return (
+      <Card variant="outlined" >
+        <CardContent>
+          <Typography >
+            <Subscription subscription={KPI_SUBSCRIPTION}
+              variables={{ campaignId: this.state.campaignId, kpiName: this.state.kpiName }}
+              shouldResubscribe={true} onSubscriptionData={this.wiggle}>
+              {
+                ({ data, loading }) => {
+                  if (data) {
+
+                    return (data.kpiUpdate.value);
+                  }
+                  return (this.state.initialValue);
                 }
-                return (initialValue);
               }
-            }
-          </Subscription >
+            </Subscription >
 
-        </Typography>
-      </CardContent>
-    </Card >
-  )
-};
+          </Typography>
+        </CardContent>
+      </Card >
+    );
+  }
+}
+
+
