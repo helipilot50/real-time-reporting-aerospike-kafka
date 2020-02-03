b5d56e9fec1ad3e368c505532cf1bbbed2fc26a1
Peter Milne <Peter.Milne@adform.com>
2020-02-03 15:43:38 +0100

    no message

--- campaign-ui/src/components/Kpi.js
+++ campaign-ui/src/components/Kpi.js
@@ -1,32 +1,7 @@
 import React, { Component } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { Subscription } from 'react-apollo';
-import { headShake } from 'react-animations';
-import { StyleSheet, css } from 'aphrodite';
 import gql from 'graphql-tag';
-
-
-const styles = StyleSheet.create({
-  headShake: {
-    animationName: headShake,
-    animationDuration: '1s'
-  }
-});
-
-const useStyles = makeStyles(theme => ({
-  card: {
-    height: 45,
-    width: 70,
-  },
-  // control: {
-  //   padding: theme.spacing(2),
-  // },
-}));
-
-
 
 const KPI_SUBSCRIPTION = gql`
 subscription kpiUpdate($campaignId: ID!, $kpiName:String!){
@@ -37,7 +12,6 @@ subscription kpiUpdate($campaignId: ID!, $kpiName:String!){
   }
 }
 `;
-
 
 export class Kpi extends Component {
   constructor(props) {
@@ -59,28 +33,23 @@ export class Kpi extends Component {
   }
 
   render() {
-
+    const { startAttention } = this.state
+    const type = startAttention ? 'secondary' : 'inherit';
     return (
-      <Card variant="outlined" >
-        <CardContent>
-          <Typography >
-            <Subscription subscription={KPI_SUBSCRIPTION}
-              variables={{ campaignId: this.state.campaignId, kpiName: this.state.kpiName }}
-              shouldResubscribe={true} onSubscriptionData={this.attention}>
-              {
-                ({ data, loading }) => {
-                  if (data) {
-
-                    return (data.kpiUpdate.value);
-                  }
-                  return (this.state.initialValue);
-                }
+      <Typography color={type}>
+        <Subscription subscription={KPI_SUBSCRIPTION}
+          variables={{ campaignId: this.state.campaignId, kpiName: this.state.kpiName }}
+          shouldResubscribe={true} onSubscriptionData={this.attention}>
+          {
+            ({ data, loading }) => {
+              if (data) {
+                return (data.kpiUpdate.value);
               }
-            </Subscription >
-
-          </Typography>
-        </CardContent>
-      </Card >
+              return (this.state.initialValue);
+            }
+          }
+        </Subscription >
+      </Typography>
     );
   }
 }
